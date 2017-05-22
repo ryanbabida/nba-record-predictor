083e39ff86ae6dc4a5628fadb19a6b4ef872861b
ryanbabida <ryan.babida@gmail.com>
2017-05-22 13:33:39 -0700

    fixed some styling

--- data_processing/grad_descent.py
+++ data_processing/grad_descent.py
@@ -1,20 +1,23 @@
-import numpy as np 
+import numpy as np
 
-def mse(w, features, target): 
+
+def mse(w, features, target):
 	s = 0.0
-	for i in range(len(features)): 
+	for i in range(len(features)):
 		s += (pow((np.dot(features[i], w) - target[i]), 2))
 	s = s / (float(len(features) - 2))
 	s = np.sqrt(s)
 	return s
 
+
 def predict(sample, w):
 	return np.dot(sample, w)
 
-def gradient_descent(a, x, y, m, num_iter): 
+
+def gradient_descent(a, x, y, m, num_iter):
 	e = list()
 	w = np.matrix(x.shape[1] * [0.0]).T
-	for i in range(num_iter): 
+	for i in range(num_iter):
 		for j in range(len(x)):
 			# Calculate Hypothesis
 			h = np.dot(x[j], w)
@@ -28,9 +31,3 @@ def gradient_descent(a, x, y, m, num_iter):
 			# Update Weight
 			w = w - a * grad
 	return w, e
-
-
-
-
-
-
